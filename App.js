ea94f622f2364f20b9505c63e333636ebf0d7690
Sina <sina.dorostkar@chrysalisloyalty.com>
2021-12-18 16:57:17 +0000

    implementing navigation stach

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import HomeScree from "./screens/HomeScree";
+import MapScreen from "./screens/MapScreen";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
-      <View>
-        <HomeScree />
-      </View>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScree}
+              options={{ headerShow: false }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShow: false }}
+            />
+          </Stack.Navigator>
+          {/* <HomeScree /> */}
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
