d33c48513c064e5d755e6a1e60726ccb562bf978
Sina <sina.dorostkar@chrysalisloyalty.com>
2021-12-18 16:56:03 +0000

    Adding logo and NavOptions to home

---
+++ screens/HomeScree.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
+
+const HomeScree = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+        <NavOptions />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScree;
+
+const styles = StyleSheet.create({
+  text: {
+    color: "blue",
+  },
+});
