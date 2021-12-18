ec9976bb6f66e0247b724f43c139d525d844896c
Sina <sina.dorostkar@chrysalisloyalty.com>
2021-12-18 15:54:54 +0000

    adding home screen with uber logo

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import HomeScree from "./screens/HomeScree";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Lets build uber</Text>
+      <View>
+        <HomeScree />
       </View>
     </Provider>
   );
