1bfd0e0ed2e8c4c12a2f48d048538632171c42dc
Sina <sina.dorostkar@chrysalisloyalty.com>
2021-12-18 14:22:30 +0000

    adding store to provider

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 export default function App() {
   return (
-    <Provider>
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>Lets build uber</Text>
       </View>
