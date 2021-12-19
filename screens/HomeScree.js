27f899f50b4dd6f447883c77fe7fcf545150fa1a
Sina <sina.dorostkar@chrysalisloyalty.com>
2021-12-19 14:22:02 +0000

    Adding auto complete google place search

--- screens/HomeScree.js
+++ screens/HomeScree.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import tw from "tailwind-react-native-classnames";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {useDispatch} from 'react-redux'
+
+import {setOrigin, setDestination} from "../redux/slices/navSlice"
+import { GOOGLE_MAPS_APIKEY } from "@env";
 import NavOptions from "../components/NavOptions";
 
+
 const HomeScree = () => {
+  const dispatch = useDispatch()
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -13,6 +20,45 @@ const HomeScree = () => {
             uri: "https://links.papareact.com/gzs",
           }}
         />
+        <GooglePlacesAutocomplete
+        placeholder="Where From?"
+        styles={{
+          container: {flex: 0},
+          textInput:{
+            fontSize:18
+          }
+        }}
+        fetchDetails={true}
+        onPress={(data, details = null) =>{
+          dispatch(setOrigin({
+            location:details.geometry.location,
+            description: data.description
+          }))
+          dispatch(setDestination(null))
+        }}
+        enablePoweredByContainer={false}
+        query={{key:GOOGLE_MAPS_APIKEY, language:'en'}}
+        nearbyPlacesAPI='GooglePlacesSearch'
+        debounce={400}
+        />
+            {/* <GooglePlacesAutocomplete
+                styles={{
+                  container: {flex: 0},
+                  textInput:{
+                    fontSize:18
+                  }
+                }}
+      placeholder='Search'
+      onPress={(data, details = null) => {
+        // 'details' is provided when fetchDetails = true
+        debugger
+        console.log(data, details);
+      }}
+      query={{
+        key: 'YOUR API KEY',
+        language: 'en',
+      }}
+    /> */}
         <NavOptions />
       </View>
     </SafeAreaView>
