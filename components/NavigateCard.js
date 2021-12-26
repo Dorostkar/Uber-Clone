e6cf88e6e4f52a3da7beab9659ab9be664327cd3
Sina <sina.dorostkar@chrysalisloyalty.com>
2021-12-26 13:54:48 +0000

    adding rides and eats menu at the bottom

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { Text, SafeAreaView, View, StyleSheet } from "react-native";
+import {
+  Text,
+  SafeAreaView,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import tw from "tailwind-react-native-classnames";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { useNavigation } from "@react-navigation/native";
+import { Icon } from "react-native-elements";
 
 import NavFavourites from "../components/NavFavourites";
 import { GOOGLE_MAPS_APIKEY } from "@env";
@@ -41,8 +48,31 @@ const NavigateCard = () => {
               navigation.navigate("RideOptionsCard");
             }}
           />
-          <NavFavourites />
         </View>
+        <NavFavourites />
+      </View>
+      <View
+        style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}
+      >
+        <TouchableOpacity
+          onPress={() => navigation.navigate("RideOptionsCard")}
+          style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}
+        >
+          <Icon name="car" type="font-awesome" color="white" size={16} />
+          <Text style={tw`text-white text-center`}>Rides</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}
+        >
+          <Icon
+            name="fast-food-outline"
+            type="ionicon"
+            color="black"
+            size={16}
+          />
+          <Text style={tw`text-center`}>Eats</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
