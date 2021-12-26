c681b4310549242f2647d94ca5d1774070350a0f
Sina <sina.dorostkar@chrysalisloyalty.com>
2021-12-26 11:45:17 +0000

    making faviourites places

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -6,6 +6,8 @@ import {
   Text,
   View,
 } from "react-native";
+import { Icon } from "react-native-elements";
+import tw from "tailwind-react-native-classnames";
 
 const data = [
   {
@@ -27,11 +29,23 @@ const NavFavourites = () => {
     <FlatList
       data={data}
       keyExtractor={(item) => item.id}
-      renderItem={({ item }) => (
-        <TouchableOpacity>
-          <Text>You</Text>
-        </TouchableOpacity>
-      )}
+      renderItem={({ item: { location, destination, icon } }) => {
+        return (
+          <TouchableOpacity style={tw`flex-row items-center p-5`}>
+            <Icon
+              style={tw`mr-4 rounded-full bg-gray-300 p-3`}
+              name={icon}
+              type="ionicon"
+              color="white"
+              size={18}
+            />
+            <View>
+              <Text style={tw`font-semibold text-lg`}>{location}</Text>
+              <Text style={tw`text-gray-500`}>{destination}</Text>
+            </View>
+          </TouchableOpacity>
+        );
+      }}
     />
   );
 };
